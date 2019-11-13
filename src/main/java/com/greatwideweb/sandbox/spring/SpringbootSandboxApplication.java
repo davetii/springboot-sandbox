30b0f0cfad0a4f66f1927debcb0ed870a693b4a4
Dave Turner <davetii@gmail.com>
2019-11-12 20:58:49 -0500

    initial commit

---
+++ src/main/java/com/greatwideweb/sandbox/spring/SpringbootSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.greatwideweb.sandbox.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSandboxApplication.class, args);
+	}
+
+}
